985bebf57511da3fe095a457b213b50740d96ee7
jeongyun <jeong.yun@tukorea.ac.kr>
2024-01-13 19:14:13 +0900

    feat : create splash page UI prototype

---
+++ MangoDiary/src/components/SplashPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="splash-page">
+    <img src="/images/logo.png" class="logo">
+    <h4 @click="redirectToCalendar" class="start">Touch to Start</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToCalendar() {
+      this.$router.push('/calendar');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.splash-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 844px;
+  width: 390px;
+}
+
+.logo {
+  margin-top: 200px;
+  max-width: 100%;
+  height: auto;
+}
+
+.start {
+  text-align: center;
+  margin-top: 300px;
+}
+</style>
---
+++ MangoDiary/src/router.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+    {path: "/",
+    component: () => import("./components/SplashPage.vue"),},
+    {path: "/calendar",
+    component: () => import("./components/CalendarPage.vue")},
+    {path: "/writing",
+    component: () => import("./components/WritePage.vue")},
+    {path: "/statistics",
+    component: () => import("./components/StatisticsPage.vue")},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
