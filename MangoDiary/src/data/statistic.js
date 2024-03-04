de58dcfe1f98d72960e54126fea0c159b513e880
jeongyun <jeong.yun@tukorea.ac.kr>
2024-03-04 11:29:56 +0900

    feat : remove emoji name data and add emoji id data

--- MangoDiary/src/data/statistic.js
+++ MangoDiary/src/data/statistic.js
@@ -2,35 +2,35 @@ const statisticsData = [
     {
         month: '화나는 달',
         name: '화남',
-        emoji: 'angry',
+        emoji_id: '2cc3a655-bdb5-4c24-9f83-c7e531d53b1c',
         count: 0,
         comment: "어려움과 화남을 뒤로하고, 새로운\n달을 긍정적으로 맞이해봐요"
     },
     {
         month: '우울한 달',
         name: '우울',
-        emoji: 'depressed',
+        emoji_id: "c944cb7a-b6da-4b28-9464-b7c3eb2c9e3a",
         count: 0,
         comment: "어려움과 우울함을 뒤로하고,\n새로운 달을 긍정적으로 맞이해봐요"
     },
     {
         month: '기쁜 달',
         name: '기쁨',
-        emoji: 'pleased',
+        emoji_id: "15b61694-d624-4fe0-92ac-d72c4dbaba1a",
         count: 0,
         comment: "좋았던 이번 달처럼, 새로운 달을\n긍정적으로 맞이해봐요"
     },
     {
         month: '행복한 달',
         name: '행복',
-        emoji: 'happy',
+        emoji_id: "cd312fb7-257e-41cb-8bc4-54c44395858e",
         count: 0,
         comment: "행복했던 이번 달처럼, 새로운 달을\n긍정적으로 맞이해봐요"
     },
     {
         month: '슬픈 달',
         name: '슬픔',
-        emoji: 'sad',
+        emoji_id: "5b29f9bd-4ab8-42ac-987a-d154578f23b5",
         count: 0,
         comment: "어려움과 슬픔을 뒤로하고, 새로운\n달을 긍정적으로 맞이해봐요"
     },
